e88a45eabf4cb6bb595b76ab319ef22fd45f288e
DESKTOP-EB9MSBQ\Admin <xht18721626070@gmail.com>
2018-10-11 16:57:05 +0800

    user ,migrate和注册验证

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar');
+            $table->string('confirmation_token');
+            $table->smallInteger('is_active')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
