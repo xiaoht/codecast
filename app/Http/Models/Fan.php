9b55e4ea79dc1fa2e508ce57f55c8bbb2284b1f7
DESKTOP-EB9MSBQ\Admin <xht18721626070@gmail.com>
2018-11-09 17:23:16 +0800

    fan

--- app/Http/Models/Fan.php
+++ app/Http/Models/Fan.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Models;
 
+use App\User;
+
 class Fan extends Model
 {
-    //
+    /**
+     * 粉丝用户
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function fuser()
+    {
+        return $this->hasOne(User::class, 'id', 'fan_id');
+    }
+
+    /**
+     * 关注用户
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function suser()
+    {
+        return $this->hasOne(User::class, 'id', 'star_id');
+    }
 }
