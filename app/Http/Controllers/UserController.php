b81b8c2e13ec306bd8eb1f105d7381ea0b9b8d2c
DESKTOP-EB9MSBQ\Admin <xht18721626070@gmail.com>
2018-11-23 17:47:18 +0800

    个人中心

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,13 @@ class UserController extends Controller
 
     public function home(User $user)
     {
-        return view('user.home');
+        $user = User::withCount(['stars', 'fans', 'posts'])->find($user->id);
+        $posts = $user->posts()->take(10)->get();
+        $stars = $user->stars();
+        $susers = User::whereIn('id', $stars->plunk('star_id'))->withCount(['stars', 'fans', 'posts'])->get();
+        $fans = $user->fans();
+        $fusers = User::whereIn('id', $fans->plunk('fan_id'))->withCount(['stars', 'fans', 'posts'])->get();
+        return view('user.home', compact('user', 'posts', 'fusers', 'susers'));
     }
 
     public function fan()
