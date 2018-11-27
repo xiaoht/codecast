e39589d09c120bacd47d4cea0a8f4688acdb53c5
xiaoht <1719299361@qq.com>
2018-11-27 10:22:14 +0800

    个人中心

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,11 +15,12 @@ class UserController extends Controller
     {
         $user = User::withCount(['stars', 'fans', 'posts'])->find($user->id);
         $posts = $user->posts()->take(10)->get();
+        $comments = $user->comments()->take(10)->get();
         $stars = $user->stars();
-        $susers = User::whereIn('id', $stars->plunk('star_id'))->withCount(['stars', 'fans', 'posts'])->get();
+        $susers = User::whereIn('id', $stars->pluck('star_id'))->withCount(['stars', 'fans', 'posts'])->get();
         $fans = $user->fans();
-        $fusers = User::whereIn('id', $fans->plunk('fan_id'))->withCount(['stars', 'fans', 'posts'])->get();
-        return view('user.home', compact('user', 'posts', 'fusers', 'susers'));
+        $fusers = User::whereIn('id', $fans->pluck('fan_id'))->withCount(['stars', 'fans', 'posts'])->get();
+        return view('user.home', compact('user', 'posts', 'comments', 'fusers', 'susers'));
     }
 
     public function fan()
